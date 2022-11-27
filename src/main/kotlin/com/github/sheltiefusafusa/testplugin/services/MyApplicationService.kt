52e0a7b6bd03e8f975fe38ba6545c8646179cbaa
GitHub Action <action@github.com>
2022-11-27 23:07:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/sheltiefusafusa/testplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sheltiefusafusa/testplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sheltiefusafusa.testplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sheltiefusafusa.testplugin.MyBundle
 
 class MyApplicationService {
 
